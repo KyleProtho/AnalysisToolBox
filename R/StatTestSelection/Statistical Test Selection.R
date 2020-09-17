aae4a122e444550d5b49da16cdb983669b0209ca
Kyle Protho <Onenonlykpro@gmail.com>
2020-09-16 20:22:18 -0400

    Update progress

--- R/StatTestSelection/Statistical Test Selection.R
+++ R/StatTestSelection/Statistical Test Selection.R
@@ -13,6 +13,12 @@ outcome_variable_type_1 = rstudioapi::showPrompt(
      message = prompt_text, 
      default = ""
 )
+if (outcome_variable_type_1 == "C") {
+        is_categorical_outcome = TRUE
+} else {
+        is_categorical_outcome = FALSE
+}
+
 
 # If quantitative outcome selected, determine discrete or continuous --------------
 if (outcome_variable_type_1 == "Q") {
@@ -29,11 +35,13 @@ if (outcome_variable_type_1 == "Q") {
 }
 if (outcome_variable_type_1 == "Q" & outcome_variable_type_2 == "C") {
      is_continuous_outcome = TRUE
+     is_quantitative_outcome = TRUE
 } else {
      is_continuous_outcome = FALSE
 }
 if (outcome_variable_type_1 == "Q" & outcome_variable_type_2 == "C") {
      is_discrete_outcome = TRUE
+     is_quantitative_outcome = TRUE
 } else {
      is_discrete_outcome = FALSE
 }
@@ -85,22 +93,44 @@ if (explanatory_variable_type_1 == "C") {
      is_groups = FALSE
 }
 
-# If cateogrical variable selected, determine number of groups ---------------------
-prompt_text = paste("You've selected a categorical explanatory variable.",
-                    "Next, you'll have to consider the number of groups you are comparing.",
-                    "The appropriate statistical test for you analysis most often depends on whether you are comparing 2 or more than 2 groups.",
-                    "--  Enter 'Y' if you are comparing 2 groups.",
-                    "--  Enter 'N' if you are comparing 3 or more groups.",
-                    sep = "\n")
-is_two_groups = rstudioapi::showPrompt(
-     title = "Are You Comparing 2 Groups?",
-     message = prompt_text, 
-     default = ""
-)
+# If categorical explanatory variable selected, determine number of groups if outcome is quantitative ---------------------
+if (explanatory_variable_type_1 == "C" & outcome_variable_type_1 == "Q") {
+        prompt_text = paste("You've selected a categorical explanatory variable.",
+                            "Next, you'll have to consider the number of groups you are comparing.",
+                            "The appropriate statistical test for your analysis most often depends on whether you are comparing 2 or more than 2 groups.",
+                            "--  Enter 'Y' if you are comparing 2 groups.",
+                            "--  Enter 'N' if you are comparing 3 or more groups.",
+                            sep = "\n")
+        is_two_groups = rstudioapi::showPrompt(
+                title = "Are You Comparing 2 Groups?",
+                message = prompt_text, 
+                default = ""
+        )
+}
 if (is_two_groups == "Y") {
      is_two_groups = TRUE
 } else {
      is_two_groups = FALSE
+}
+
+# If quantitative explanatory variable selected, determine number of predictor variables ---------------------
+if (explanatory_variable_type_1 == "Q") {
+        prompt_text = paste("You've selected a quantitative explanatory variable.",
+                            "Next, you'll have to consider the number of predictors you'd like to study.",
+                            "The appropriate regression model for your analysis most often depends on whether you are analyzing 1 or more than 1 predictor variable.",
+                            "--  Enter 'Y' if you are analyzing 1 predictor (or independent variable).",
+                            "--  Enter 'N' if you are analyzing 2 or more predictors (or independent variables).",
+                            sep = "\n")
+        is_one_predictor = rstudioapi::showPrompt(
+                title = "Are You Analyzing 1 Predictor?",
+                message = prompt_text, 
+                default = ""
+        )
+}
+if (is_one_predictor == "Y") {
+        is_one_predictor = TRUE
+} else {
+        is_one_predictor = FALSE
 }
 
 # Determine independent or related observations ------------------------------------
@@ -118,6 +148,7 @@ if (observation_type == "R") {
 } else {
      is_related_obs = FALSE
 }
+
 
 # Show recommendations ----------------------------------------------------
 # T-Test (and alternatives) ------------------------------------------------------------------
@@ -178,4 +209,103 @@ if (is_continuous_outcome & is_groups & is_two_groups == FALSE & is_related_obs)
         rstudioapi::showQuestion(
                 title = "Statistical Test Recommendation",
                 message = prompt_text)
+}
+
+
+# Chi-square test (and alternatives) ----------------------------------------------------------
+if (is_categorical_outcome & is_groups & is_related_obs == FALSE) {
+        prompt_text = paste("Since you are comparing a categorical outcome across independent groups, then any of the following are options for you:",
+                            "",
+                            "A)  Chi Square Test.",
+                            "However, if one of your group/outcome counts is less than 5, then the Fisher Exact Test is a better alternative.",
+                            "",
+                            "B)  Difference in Risk",
+                            "",
+                            "C)  Relative Risk",
+                            "",
+                            sep = "\n")
+        rstudioapi::showQuestion(
+                title = "Statistical Test Recommendation",
+                message = prompt_text) 
+}
+
+# McNemar's test (and alternatives) -----------------------------------------
+if (is_categorical_outcome & is_groups & is_related_obs) {
+        prompt_text = paste("Since you are comparing a categorical outcome across related groups, then the McNemar's Test might be best.",
+                            "However, if one of your group/outcome counts is less than 5, then the McNemar's Exact Test is a better alternative.",
+                            sep = "\n")
+        rstudioapi::showQuestion(
+                title = "Statistical Test Recommendation",
+                message = prompt_text) 
+}
+
+
+# Rate Ratio --------------------------------------------------------------
+if (is_discrete_outcome & is_groups & is_related_obs == FALSE) {
+        prompt_text = paste("Since you are comparing a discrete outcome (counts) across independent groups, then Rate Ratios and/or Odds Ratios might be best.",
+                            "The 'epitools' package is useful for this.",
+                            "A helpful note: When an event is rare (less 10% of sample or population experiences it), then the rate ratio and odds ratio become increasingly similar.",
+                            "Otherwise, odds ratios can exaggerate the frequency of some event compared to rate ratios. Keep that in mind when interpreting your results.",
+                            sep = "\n")
+        rstudioapi::showQuestion(
+                title = "Statistical Test Recommendation",
+                message = prompt_text) 
+}
+
+# Kaplan-Meier statistics --------------------------------------------------------------
+if (is_time_to_event_outcome & is_groups & is_related_obs == FALSE) {
+        prompt_text = paste("Since you are comparing time-to-event across independent groups, then it is best if you use one of the following methods:",
+                            "A)  Kaplan-Meier statistics (also known as Survival Analysis)",
+                            "B)  Rate Ratios and/or Odds Ratios (the 'epitools' package is useful for this)",
+                            "",
+                            "A helpful note: When an event is rare (less 10% of sample or population experiences it), then the rate ratio and odds ratio become increasingly similar.",
+                            "Otherwise, odds ratios can exaggerate the frequency of some event compared to rate ratios. Keep that in mind when interpreting your results.",
+                            sep = "\n")
+        rstudioapi::showQuestion(
+                title = "Statistical Test Recommendation",
+                message = prompt_text) 
+}
+
+# Linear regression -------------------------------------------------------
+if (is_quantitative_outcome & is_groups == FALSE & is_one_predictor & is_related_obs == FALSE) {
+        prompt_text = paste("Since you are analyzing the relationship between 1 dependent and 1 independent variable, then it is best if you use one of the following methods:",
+                            "",
+                            "A)  Pearson's correlation coefficent, if you are not interested in the predictive relationship between the two variables",
+                            "However, if sample size is small (less than 100) AND the outcome is not normally distributed, then you may get misleading results in your Pearson's correlation coeffiencent.",
+                            "If the above scenario is true, consider taking one of the following corrective actions:",
+                            "--  Use the non-parametric Spearman's correlation coefficient test instead, which finds the relationship between the ranks of each data point rather than their absoluate values.",
+                            "--  Transforming your variable(s) (e.g. take the natural log or square root of them) in an effort to make them more normally distributed, then running the Pearson's correlation coefficent test on that transformed variable. Just remember that the estimates of Pearson's correlation coefficient will be on the transformed variable, not the original values.",
+                            "",
+                            "B)  Linear regression, if you are interested in the predictive relationship between the outcome (dependent) and the predictor (independent) variables",
+                            "However, if one of the following scenarios is true, then you may get misleading results in your Linear regression:",
+                            "--  The relationship between the outcome and the predictor is not linear",
+                            "--  The outcome is not distributed normally at each value of the predictor. In other words, the random error does not follow a normal distribution",
+                            "--  The variance of the outcome at every value of the predictor is not the same. In other words, there is no homogeneity of variances.",
+                            "If any of the first 3 scenarios are true, then consider transforming your variable(s) (e.g. take the natural log or square root of them) in an effort to make them more normally distributed, then running the Linear Regression on those transformed variables. Just remember to transform the estimates back to their original form, or else you'll confuse your reader.",
+                            sep = "\n")
+        rstudioapi::showQuestion(
+                title = "Statistical Test Recommendation",
+                message = prompt_text) 
+}
+
+# Multiple linear regression ----------------------------------------------
+if (is_quantitative_outcome & is_groups == FALSE & is_one_predictor == FALSE & is_related_obs == FALSE) {
+        prompt_text = paste("Since you are analyzing the relationship between 1 dependent and 2 or more independent variable, then it is best if you use one of the following methods:",
+                            "",
+                            "A)  Pearson's correlation coefficent matrix, if you are not interested in the predictive relationship between the dependent and independent variables",
+                            "However, if sample size is small (less than 100) AND the outcome is not normally distributed, then you may get misleading results in your Pearson's correlation coeffiencent.",
+                            "If the above scenario is true, consider taking one of the following corrective actions:",
+                            "--  Use the non-parametric Spearman's correlation coefficient test instead, which finds the relationship between the ranks of each data point rather than their absoluate values.",
+                            "--  Transforming your variable(s) (e.g. take the natural log or square root of them) in an effort to make them more normally distributed, then running the Pearson's correlation coefficent test on that transformed variable. Just remember that the estimates of Pearson's correlation coefficient will be on the transformed variable, not the original values.",
+                            "",
+                            "B)  Multiple linear regression, if you are interested in the predictive relationship between the independent and the dependent variables",
+                            "However, if one of the following scenarios is true, then you may get misleading results in your Linear regression:",
+                            "--  The relationship between the outcome and the predictors is not linear",
+                            "--  The outcome is not distributed normally at each value of the predictors. In other words, the random error does not follow a normal distribution",
+                            "--  The variance of the outcome at every value of the predictors is not the same. In other words, there is no homogeneity of variances.",
+                            "If any of the first 3 scenarios are true, then consider transforming your variable(s) (e.g. take the natural log or square root of them) in an effort to make them more normally distributed, then running the Linear Regression on those transformed variables. Just remember to transform the estimates back to their original form, or else you'll confuse your reader.",
+                            sep = "\n")
+        rstudioapi::showQuestion(
+                title = "Statistical Test Recommendation",
+                message = prompt_text) 
 }
