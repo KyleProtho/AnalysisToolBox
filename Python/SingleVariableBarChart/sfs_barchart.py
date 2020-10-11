3d700d596838fc9f89f76f820a951947a8335427
Kyle Protho <onenonlykpro@gmail.com>
2020-10-11 11:27:08 -0400

    Fixed SFS Histogram :bug:

---
+++ Python/SingleVariableBarChart/sfs_barchart.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 09:48:33 2020
+
+@author: oneno
+"""
+
+# Load packages
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Create histogram function
+def sfs_barchart(data,
+                  categorical_variable,
+                  readable_label_for_categorical_variable = None,
+                  folder_to_save_plot = None,
+                  fill_color = None):
+    # Create readable label for qualitative variable if one isn't specified
+    if readable_label_for_categorical_variable == None:
+        readable_label_for_categorical_variable = categorical_variable
+    
+    # Create histogram title
+    title_for_plot = "Bar Chart - " + readable_label_for_categorical_variable
+    
+    # Set style theme
+    sns.set_style("whitegrid")
+    
+    # Generate bar chart
+    fig = plt.figure()
+    if fill_color == None:
+        ax = sns.countplot(data = data,
+                           x = categorical_variable, 
+                           palette = "Spectral")
+    else:
+        ax = sns.countplot(data = data,
+                           x = categorical_variable,
+                           color = fill_color)
+    ax.set_title(title_for_plot,
+                 loc = 'left')
+    ax.set_xlabel(readable_label_for_categorical_variable)
+    
+    # Save plot to folder if one is specified
+    if folder_to_save_plot != None:
+        try:
+            plot_filepath = str(folder_to_save_plot) + title_for_plot + ".png"
+            plot_filepath = os.path.normpath(plot_filepath)
+            fig.savefig(plot_filepath)
+        except:
+            print("The filpath you entered to save your plot is invalid. Plot not saved.")
+    
+    # Show plot
+    plt.show()
+    
+    # Clear plot
+    plt.clf()
+
--- Python/SingleVariableHistogram/sfs_histogram.py
+++ Python/SingleVariableHistogram/sfs_histogram.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  2 14:10:50 2020
+Created on Wed Oct  7 07:49:58 2020
 
-@author: Kyle Protho
+@author: oneno
 """
 
-# Load packages
 import pandas as pd
-import matplotlib as plt
+import os
+from matplotlib import pyplot as plt
 import seaborn as sns
-from qbstyles import mpl_style
-from os import path
 
+# Create histogram function
 def sfs_histogram(data,
                   quantitative_variable,
                   readable_label_for_quantitative_variable = None,
                   folder_to_save_plot = None,
-                  fill_color = "#3269a8"):
+                  fill_color = "#3269a8",
+                  is_discrete_variable = False):
     # Calculate best number of bins and bin width/size
     sd_of_var = data[quantitative_variable].std()
     min_of_var = data[quantitative_variable].min()
     max_of_var = data[quantitative_variable].max()
-    n = data[quantitative_variable].count()
+    n = len(data[quantitative_variable].index)
     ## number of bins
     numerator = (max_of_var - min_of_var) * (n ** (1/3))
     denominator = 3.5 * sd_of_var
     number_of_bins = numerator / denominator
-    number_of_bins = round(number_of_bins, 0)
+    try:
+        number_of_bins = round(number_of_bins, 0)
+        number_of_bins = int(number_of_bins)
+    except:
+        number_of_bins = 1
     if number_of_bins < 1:
         number_of_bins = 1
     ## bin width 
@@ -34,7 +38,6 @@ def sfs_histogram(data,
     denominator = n ** (1/3)
     bin_width = numerator / denominator
     bin_width = round(bin_width, 2)
-    
     # Create readable label for quantitative variable if one isn't specified
     if readable_label_for_quantitative_variable == None:
         readable_label_for_quantitative_variable = quantitative_variable
@@ -46,28 +49,39 @@ def sfs_histogram(data,
     sns.set_style("whitegrid")
     
     # Create histogram
-    p = sns.histplot(data = data,
-                     x = quantitative_variable,
-                     stat = "count",
-                     color = fill_color,
-                     bins = number_of_bins,
-                     binwidth = bin_width)
-    p.set(title = title_for_plot,
-          xlabel = str(readable_label_for_quantitative_variable))
+    fig = plt.figure()
+    if is_discrete_variable:
+        bin_width = round(bin_width, 0)
+        if bin_width < 1:
+            bin_width = 1
+        ax = sns.histplot(data = data,
+                          x = quantitative_variable,
+                          stat = "count",
+                          color = fill_color, 
+                          bins = number_of_bins,
+                          binwidth = bin_width)
+    else:
+        ax = sns.histplot(data = data,
+                          x = quantitative_variable,
+                          stat = "count",
+                          color = fill_color,
+                          bins = number_of_bins)
+    ax.set_title(title_for_plot,
+                 loc = 'left')
+    ax.set_xlabel(readable_label_for_quantitative_variable)
     
     # Save plot to folder if one is specified
     if folder_to_save_plot != None:
         try:
             plot_filepath = str(folder_to_save_plot) + title_for_plot + ".png"
-            plot_filepath = path.normpath(plot_filepath)
-            p.savefig(plot_filepath)
+            plot_filepath = os.path.normpath(plot_filepath)
+            fig.savefig(plot_filepath)
         except:
             print("The filpath you entered to save your plot is invalid. Plot not saved.")
+    
+    # Show plot
+    plt.show()
+    
+    # Clear plot
+    plt.clf()
 
-
-# Import test data (mtcars)
-df_mtcars = pd.read_csv('https://gist.githubusercontent.com/ZeccaLehn/4e06d2575eb9589dbe8c365d61cb056c/raw/64f1660f38ef523b2a1a13be77b002b98665cdfe/mtcars.csv')
-df_mtcars.rename(columns = {'Unnamed: 0':'brand'}, 
-                 inplace=True)
-sfs_histogram(data = df_mtcars,
-              quantitative_variable = "mpg")
