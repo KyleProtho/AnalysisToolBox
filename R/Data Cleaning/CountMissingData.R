f02be49bc1022a87d88ac3816b4d780c6297453c
Kyle Protho <onenonlykpro@gmail.com>
2022-03-14 07:52:10 -0400

    Update CountMissingData.R

--- R/Data Cleaning/CountMissingData.R
+++ R/Data Cleaning/CountMissingData.R
@@ -4,17 +4,7 @@ library(ggplot2)
 
 # Define function
 CountMissingData = function(dataframe,
-                            show_incomplete_case_plot = TRUE,
                             show_missing_count_by_variable_plot = TRUE) {
-  # If request, show plot of incomplete cases
-  if (show_incomplete_case_plot) {
-    p = gg_miss_case(dataframe,
-                     show_pct = TRUE) + 
-      labs(x = "Number of Cases") +
-      theme_minimal()
-    plot(p)
-  }
-  
   # Iterate through columns and count null values
   for (variable in colnames(dataframe)) {
     missing_values = sum(is.na(dataframe[[variable]]))
