75d1d49b8f2f00bee79fe0b4115655fe05aaed1e
Kyle Protho <onenonlykpro@gmail.com>
2022-06-23 10:43:22 -0400

    Updated VerifyGranularity function

--- Python/Data Cleaning/VerifyGranularity.py
+++ Python/Data Cleaning/VerifyGranularity.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Define function
 def VerifyGranularity(dataframe,
                       list_of_key_columns,
-                      keep_key_column=True):
+                      set_key_as_index=True):
     # Create key column from list of key columns
     first_col = list_of_key_columns[0]
     dataframe['Dataset Key'] = dataframe[first_col].astype(str)
@@ -17,13 +17,16 @@ def VerifyGranularity(dataframe,
     # Print row count and distinct key of Dataset Key
     print("Number of rows in dataframe:", str(len(dataframe.index)))
     print("Distinct count of Dataset Keys:", str(len(pd.unique(dataframe['Dataset Key']))))
+    if len(dataframe.index) != len(pd.unique(dataframe['Dataset Key'])):
+        print("WARNING: Unique combination of columns you listed does not equal number of rows in dataset. Try a new combination of columns or see if dataset has duplicates.")
     
     # If Key column not being kept, drop it. Otherwise, move Key column to front of DataFrame
-    if keep_key_column != True:
+    if set_key_as_index != True:
         dataframe = dataframe.drop(columns=['Dataset Key'])
     else:
-        first_column = dataframe.pop('Dataset Key')
-        dataframe.insert(0, 'Dataset Key', first_column)
+        dataframe.index = dataframe['Dataset Key']
+        dataframe = dataframe.drop(columns=['Dataset Key'])
         
     # Return dataframe
     return(dataframe)
+
