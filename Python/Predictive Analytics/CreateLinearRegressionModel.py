ab3f00d09d9a7c17bdef20f5cb999dc743c7836a
Kyle Protho <onenonlykpro@gmail.com>
2022-11-12 20:49:54 -0500

    Update CreateLinearRegressionModel.py

--- Python/Predictive Analytics/CreateLinearRegressionModel.py
+++ Python/Predictive Analytics/CreateLinearRegressionModel.py
@@ -2,9 +2,9 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import statsmodels.api as sm
 
 # Function to create linear regression model
 def CreateLinearRegressionModel(dataframe,
@@ -12,15 +12,15 @@ def CreateLinearRegressionModel(dataframe,
                                 list_of_predictors,
                                 scale_predictors=False,
                                 test_size=0.2,
+                                max_iterations=1000,
+                                learning_rate=0.0001,
+                                show_plot_of_residuals=True,
                                 random_seed=412):
     # Keep only the predictors and outcome variable
     dataframe = dataframe[list_of_predictors + [outcome_variable]].copy()
     
     # Keep complete cases
     dataframe.dropna(inplace = True)
-    
-    # Add constant to predictors
-    dataframe = sm.add_constant(dataframe)
     
     # Scale the predictors, if requested
     if scale_predictors:
@@ -31,7 +31,11 @@ def CreateLinearRegressionModel(dataframe,
         print(dataframe[list_of_predictors].std())
         
         # Scale predictors
-        dataframe[list_of_predictors] = dataframe[list_of_predictors].apply(lambda x: (x - x.mean()) / x.std())
+        dataframe[list_of_predictors] = StandardScaler().fit_transform(dataframe[list_of_predictors])
+        
+    # Show the peak-to-peak range of each predictor
+    print("\nPeak-to-peak range of each predictor:")
+    print(np.ptp(dataframe[list_of_predictors], axis=0))
     
     # Split dataframe into training and test sets
     train, test = train_test_split(
@@ -41,40 +45,63 @@ def CreateLinearRegressionModel(dataframe,
     )
     
     # Create linear regression object
-    regr = linear_model.LinearRegression()
+    regr = linear_model.SGDRegressor(
+        loss='squared_loss',
+        max_iter=max_iterations,
+        alpha=learning_rate,
+        random_state=random_seed,
+        fit_intercept=True,
+    )
     
     # Train the model using the training sets
     regr.fit(train[list_of_predictors], train[outcome_variable])
     
-    # Show summary of linear regression model
-    model_summary = sm.OLS(train[outcome_variable], train[['const'] + list_of_predictors]).fit()
-    model_summary = model_summary.summary()
-    print(model_summary)
-    
+    # Show parameters of the model
+    b_norm = regr.intercept_
+    w_norm = regr.coef_
+    print(f"\nModel parameters:                   w: {w_norm}, b:{b_norm}")
+        
     # Show the explained variance score: 
-    print("Variance score: %.2f" % regr.score(train[list_of_predictors], train[outcome_variable]))
+    print("\nVariance score: %.2f" % regr.score(train[list_of_predictors], train[outcome_variable]))
     print("Note: 1 is perfect prediction and 0 means that there is no linear relationship between X and Y.")
     
     # The mean squared error
     print("\nMean squared error: %.2f" % np.mean((regr.predict(test[list_of_predictors]) - test[outcome_variable]) ** 2))
     
-    # Plot predicted and observed outputs
-    plt.scatter(
-        test[outcome_variable], 
-        regr.predict(test[list_of_predictors]),
-        color='blue',
-        linewidth=3
-    )
-    plt.xlabel('Observed ' + outcome_variable)
-    plt.ylabel('Predicted ' + outcome_variable)
-    plt.show()
+    # Plot predicted and observed outputs if requested
+    if show_plot_of_residuals:
+        fig, ax = plt.subplots(
+            int(np.ceil(len(list_of_predictors))), 
+            1,
+            figsize=(4, int(np.ceil(len(list_of_predictors)))*4.5),
+            sharey=True
+        )
+        for i in range(len(ax)):
+            ax[i].scatter(
+                train[list_of_predictors[i]],
+                train[outcome_variable], 
+                label="Observed"
+            )
+            ax[i].set_xlabel(list_of_predictors[i])
+            ax[i].set_ylabel(outcome_variable)
+            ax[i].scatter(
+                train[list_of_predictors[i]],
+                regr.predict(train[list_of_predictors]),
+                label='Predicted'
+            ) 
+        ax[0].legend()
+        fig.suptitle("Observed and predicted outcome")
+        plt.show()
     
     # Return the model
     return regr
 
-# Test the function
-iris = pd.DataFrame(datasets.load_iris(as_frame=True).data)
-linear_reg_model = CreateLinearRegressionModel(dataframe=iris,
-                                               outcome_variable='sepal length (cm)',
-                                               list_of_predictors=['sepal width (cm)', 'petal length (cm)', 'petal width (cm)'],
-                                               scale_predictors=True)
+
+# # Test the function
+# from sklearn import datasets
+# iris = pd.DataFrame(datasets.load_iris(as_frame=True).data)
+# linear_reg_model = CreateLinearRegressionModel(dataframe=iris,
+#                                                outcome_variable='sepal length (cm)',
+#                                                list_of_predictors=['sepal width (cm)', 'petal length (cm)', 'petal width (cm)'],
+#                                                scale_predictors=True)
+
