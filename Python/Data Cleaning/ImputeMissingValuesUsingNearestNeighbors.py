6b8ce8e631ae5167c3262a9d3d73c15140cc6245
Kyle Protho <Onenonlykpro@gmail.com>
2022-12-07 13:20:18 -0500

    Create ImputeMissingValuesUsingNearestNeighbors.py

---
+++ Python/Data Cleaning/ImputeMissingValuesUsingNearestNeighbors.py
@@ -0,0 +1,34 @@
+# Load packages
+import pandas as pd
+from sklearn.impute import KNNImputer
+
+def ImputeMissingValuesUsingNearestNeighbors(dataframe,
+                                             list_of_variables,
+                                             number_of_neighbors=3,
+                                             averaging_method='uniform'):
+    # Select only the variables to impute
+    dataframe_imputed = dataframe[list_of_variables].copy()
+    
+    # Impute missing values using nearest neighbors
+    imputer = KNNImputer(n_neighbors=number_of_neighbors,
+                         weights=averaging_method)
+    
+    # Fit the imputer
+    dataframe_imputed = imputer.fit_transform(dataframe)
+    
+    # Add "- Imputed" to the variable names
+    list_new_column_names = []
+    for variable in list_of_variables:
+        variable_imputed = variable + " - Imputed"
+        list_new_column_names.append(variable_imputed)
+    
+    # Convert the imputed array to a dataframe
+    dataframe_imputed = pd.DataFrame(dataframe_imputed,
+                                     columns=list_new_column_names)
+    
+    # Bind the imputed dataframe to the original dataframe
+    dataframe = pd.concat([dataframe, dataframe_imputed], axis=1)
+    del(dataframe_imputed)
+    
+    # Return the dataframe with imputed values
+    return(dataframe)
