cb3243f98938a58e28843c0b5ff60810e04cd754
Kyle Protho <Onenonlykpro@gmail.com>
2022-12-15 11:12:16 -0500

    Update PlotCorrelationMatrix.py

--- Python/Visualizations/PlotCorrelationMatrix.py
+++ Python/Visualizations/PlotCorrelationMatrix.py
@@ -6,14 +6,14 @@ sns.set(style="white",
         context="paper")
 
 def PlotCorrelationMatrix(dataframe,
-                          list_of_predictor_variables,
+                          list_of_numeric_variables,
                           outcome_variable=None,
                           show_as_pairplot=True):
     # Select relevant variables, keep complete cases only
     if outcome_variable is None:
-        completed_df = dataframe[list_of_predictor_variables].dropna()
+        completed_df = dataframe[list_of_numeric_variables].dropna()
     else:
-        completed_df = dataframe[[outcome_variable] + list_of_predictor_variables].dropna()
+        completed_df = dataframe[[outcome_variable] + list_of_numeric_variables].dropna()
     
     # Drop Inf values
     completed_df = completed_df[np.isfinite(completed_df).all(1)]
@@ -30,7 +30,7 @@ def PlotCorrelationMatrix(dataframe,
             ax = sns.pairplot(
                 completed_df,
                 kind="reg",
-                x_vars=list_of_predictor_variables,
+                x_vars=list_of_numeric_variables,
                 y_vars=outcome_variable
             )
         ax.fig.subplots_adjust(top=0.95)
@@ -42,6 +42,14 @@ def PlotCorrelationMatrix(dataframe,
             ha='left', 
             va='top'
         )
+        # Wrap labels in each subplot
+        for ax in ax.axes.flat:
+            # x-axis labels
+            wrapped_x_labels = "\n".join(ax.get_xlabel()[j:j+30] for j in range(0, len(ax.get_xlabel()), 30))
+            ax.set_xlabel(wrapped_x_labels, horizontalalignment='center')
+            # y-axis labels
+            wrapped_y_labels = "\n".join(ax.get_ylabel()[j:j+30] for j in range(0, len(ax.get_ylabel()), 30))
+            ax.set_ylabel(wrapped_y_labels, horizontalalignment='center')
     else:
         print(completed_df.corr())
 
@@ -49,21 +57,21 @@ def PlotCorrelationMatrix(dataframe,
 # # Test the function
 # from sklearn import datasets
 # iris = pd.DataFrame(datasets.load_iris(as_frame=True).data)
-# # PlotCorrelationMatrix(
-# #     dataframe=iris,
-# #     list_of_predictor_variables=[
-# #         'sepal length (cm)',
-# #         'sepal width (cm)', 
-# #         'petal length (cm)', 
-# #         'petal width (cm)'
-# #     ]
-# # )
 # PlotCorrelationMatrix(
-#     dataframe=iris, 
-#     list_of_predictor_variables=[
+#     dataframe=iris,
+#     list_of_numeric_variables=[
+#         'sepal length (cm)',
 #         'sepal width (cm)', 
 #         'petal length (cm)', 
 #         'petal width (cm)'
-#     ],
-#     outcome_variable='sepal length (cm)',
+#     ]
 # )
+# # PlotCorrelationMatrix(
+# #     dataframe=iris, 
+# #     list_of_numeric_variables=[
+# #         'sepal width (cm)', 
+# #         'petal length (cm)', 
+# #         'petal width (cm)'
+# #     ],
+# #     outcome_variable='sepal length (cm)',
+# # )
