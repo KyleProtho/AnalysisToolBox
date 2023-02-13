86124fa538c16dbed353676e32644299794271d1
Kyle Protho <Onenonlykpro@gmail.com>
2023-02-13 10:11:40 -0500

    Update SimulateOutcomeFromMetalog.R

--- R/Simulations/SimulateOutcomeFromMetalog.R
+++ R/Simulations/SimulateOutcomeFromMetalog.R
@@ -8,8 +8,8 @@ SimulateOutcomeFromMetalog = function(metalog_distribution,
                                       name_of_variable = NULL) {
   
   # Ensure that number of terms and number of trails are integers
-  number_of_terms = integer(number_of_terms)
-  number_of_trials = integer(number_of_trials)
+  number_of_terms = as.numeric(as.integer(number_of_terms))
+  number_of_trials = as.numeric(as.integer(number_of_trials))
   
   # Generate SIP
   df_simulated = rmetalog(metalog_distribution, 
@@ -28,3 +28,24 @@ SimulateOutcomeFromMetalog = function(metalog_distribution,
   # Return simulated values
   return(df_simulated)
 }
+
+
+# # Test the function
+# iris = iris
+# hist(iris[["Petal.Length"]], col = "red", main = "Petal length - Observed values")
+# source("https://raw.githubusercontent.com/onenonlykpro/SnippetsForStatistics/master/R/Simulations/CreateMetalogDistribution.R")
+# dist_petal_length = CreateMetalogDistribution(
+#   dataframe=iris,
+#   column_name="Petal.Length",
+#   boundary_setting="b",
+#   upper_boundary=8,
+#   lower_boundary=1,
+#   plot_fitted_distributions=FALSE
+# )
+# summary(dist_petal_length)
+# sip_petal_length = SimulateOutcomeFromMetalog(
+#   metalog_distribution=dist_petal_length,
+#   number_of_terms=7,
+#   number_of_trials=10000 
+# )
+# hist(sip_petal_length[["simulation_result"]], col = "blue", main = "Petal length - Simulated values")
