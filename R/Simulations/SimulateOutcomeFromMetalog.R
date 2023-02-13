740d72b155da64d5f315bdf1b3cadfe02f575c58
Kyle Protho <Onenonlykpro@gmail.com>
2023-02-13 09:34:12 -0500

    Update SimulateOutcomeFromMetalog.R

--- R/Simulations/SimulateOutcomeFromMetalog.R
+++ R/Simulations/SimulateOutcomeFromMetalog.R
@@ -6,6 +6,11 @@ SimulateOutcomeFromMetalog = function(metalog_distribution,
                                       number_of_terms = 4,
                                       number_of_trials = 10000,
                                       name_of_variable = NULL) {
+  
+  # Ensure that number of terms and number of trails are integers
+  number_of_terms = integer(number_of_terms)
+  number_of_trials = integer(number_of_trials)
+  
   # Generate SIP
   df_simulated = rmetalog(metalog_distribution, 
                           n = number_of_trials, 
