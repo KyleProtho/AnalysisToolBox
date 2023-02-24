a58b6e8c89c3ffeb4ca48daa8114cbd6c8d44ee3
Kyle Protho <onenonlykpro@gmail.com>
2023-02-23 19:14:10 -0500

    PlotVectors function

---
+++ Python/Linear Algebra/PlotVectors.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+def PlotVectors(list_of_vectors,
+                list_of_vector_labels=None,
+                color_palette='Set2'):
+    # Iterate through the list of vectors, and if the vector is a list, convert it to a numpy array
+    for vector_index in range(len(list_of_vectors)):
+        if type(list_of_vectors[vector_index]) == list:
+            list_of_vectors[vector_index] = np.array(list_of_vectors[vector_index])
+        if len(list_of_vectors[vector_index].shape) > 2:
+            raise Exception('Vector {} has more than 2 coordinates'.format(vector_index))
+    
+    # If list_of_vector_labels is None, generate a list of labels
+    if list_of_vector_labels is None:
+        list_of_vector_labels= []
+        for vector_index in range(len(list_of_vectors)):
+            list_of_vector_labels.append('Vector {}'.format(vector_index))
+    
+    # Set the color palette
+    sns.set_palette(color_palette)
+    
+    # Plot the vectors
+    _, ax = plt.subplots(figsize=(10, 10))
+    ax.tick_params(axis='x', labelsize=14)
+    ax.tick_params(axis='y', labelsize=14)
+    # Set the x and y limits
+    x_min = min([min(vector[0]) for vector in list_of_vectors])
+    if x_min > 0:
+        x_min = 0
+    x_max = max([max(vector[0]) for vector in list_of_vectors])
+    if x_max < 0:
+        x_max = 0
+    y_min = min([min(vector[1]) for vector in list_of_vectors])
+    if y_min > 0:
+        y_min = 0
+    y_max = max([max(vector[1]) for vector in list_of_vectors])
+    if y_max < 0:
+        y_max = 0
+    ax.set_xlim([x_min-1, x_max+1])
+    ax.set_ylim([y_min-1, y_max+1])
+        
+    # Plot each vector
+    for i, v in enumerate(list_of_vectors):
+        sgn = 0.4 * np.array([[1] if i==0 else [i] for i in np.sign(v)])
+        plt.quiver(
+            v[0], 
+            v[1],
+            color=sns.color_palette()[i],
+            angles='xy', 
+            scale_units='xy', 
+            scale=1
+        )
+        ax.text(
+            v[0]-0.2+sgn[0],
+            v[1]-0.2+sgn[1], 
+            list_of_vector_labels[i],
+            fontsize=10,
+            color=sns.color_palette()[i]
+        )
+    # Add a grid
+    plt.grid()
+    # Set aspect ratio to equal
+    plt.gca().set_aspect("equal")
+    # Remove borders
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['bottom'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    
+    # Add line at x=0 and y=0
+    plt.axhline(y=0, color='k', alpha=0.25)
+    plt.axvline(x=0, color='k', alpha=0.25)
+    
+    # Show the plot
+    plt.show()
+
+
+# # Test the function
+# v = np.array([[1],[3]])
+# w = np.array([[4],[-1]])
+# v_plus_w = v + w
+# PlotVectors([v, w, v_plus_w])
