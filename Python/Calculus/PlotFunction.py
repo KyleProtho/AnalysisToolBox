cc9a88e3cbaa2b57e9e49ed44df69d65b82bf80f
Kyle Protho <Onenonlykpro@gmail.com>
2023-03-11 11:36:58 -0500

    Update PlotFunction.py

--- Python/Calculus/PlotFunction.py
+++ Python/Calculus/PlotFunction.py
@@ -8,6 +8,17 @@ def PlotFunction(f_of_x,
                  minimum_x=-10, 
                  maximum_x=10, 
                  n=100):
+    """_summary_
+    This function plots a function of x.
+
+    Args:
+        f_of_x (lambda): The function of x.
+        minimum_x (int, optional): The minimum value of x to use when plotting the function. Defaults to -10.
+        maximum_x (int, optional): The maximum value of x to use when plotting the function. Defaults to 10.
+        n (int, optional): The number of points to use when plotting the function. Defaults to 100.
+    """
+    
+    # Plot the function
     x = np.linspace(minimum_x, maximum_x, n)
     y = f_of_x(x)
     plt.plot(x, y)
