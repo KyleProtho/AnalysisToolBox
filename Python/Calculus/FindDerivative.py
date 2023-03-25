b4c5fcd029b5c52347e9c55989b9cc0f27bd4137
Kyle Protho <Onenonlykpro@gmail.com>
2023-03-25 18:36:39 -0400

    Update FindDerivative.py

--- Python/Calculus/FindDerivative.py
+++ Python/Calculus/FindDerivative.py
@@ -1,10 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import sympy
 # Set the variable x as a sympy symbol
 x = sympy.Symbol('x')
 
 def FindDerivative(f_of_x,
                    print_functions=False,
-                   return_derivative_function=False):
+                   return_derivative_function=False,
+                   plot_functions=True,
+                   minimum_x=-10, 
+                   maximum_x=10, 
+                   n=100):
     """_summary_
     This function finds the derivative of a function.
 
@@ -23,6 +29,27 @@ def FindDerivative(f_of_x,
     if print_functions:
         print("f(x):", f_of_x)
         print("f'(x):", d_f_of_x)
+        
+    # Plot the derivative function if requested
+    if plot_functions:
+        # Create x values
+        x_values = np.linspace(minimum_x, maximum_x, n)
+        # Create y values for original function using sympy
+        y_values_original = np.zeros(len(x_values))
+        for i in range(len(x_values)):
+            y_values_original[i] = f_of_x.evalf(subs={x: x_values[i]})
+        # Create y values for derivative function
+        y_values_derivative = np.zeros(len(x_values))
+        for i in range(len(x_values)):
+            y_values_derivative[i] = d_f_of_x.evalf(subs={x: x_values[i]})
+        # Plot the original function
+        plt.plot(x_values, y_values_original, label="f(x)")
+        # Plot the derivative function
+        plt.plot(x_values, y_values_derivative, label="f'(x)")
+        # Add a legend
+        plt.legend()
+        # Show the plot
+        plt.show()
     
     # Return the derivative function if requested
     if return_derivative_function:
@@ -32,12 +59,8 @@ def FindDerivative(f_of_x,
 # # Test the function
 # FindDerivative(f_of_x=x**3 + 2*x**2 + x + 1,
 #                return_derivative_function=True)
-
-# # import numpy as np
 # # FindDerivative(f_of_x=x * np.e**x,
 # #                return_derivative_function=True)
-
-# # import numpy as np
 # # der_f_of_x = FindDerivative(
 # #     f_of_x=np.e**(2*x),
 # #     return_derivative_function=True
