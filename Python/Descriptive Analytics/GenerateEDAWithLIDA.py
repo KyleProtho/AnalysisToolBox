030f080d92de13e28dd4f5ac13c75aecf00f8b59
Kyle Protho <Onenonlykpro@gmail.com>
2023-08-27 09:42:24 -0400

    Update GenerateEDAWithLIDA.py

--- Python/Descriptive Analytics/GenerateEDAWithLIDA.py
+++ Python/Descriptive Analytics/GenerateEDAWithLIDA.py
@@ -11,11 +11,17 @@ import seaborn as sns
 # Declare function
 def GenerateEDAWithLIDA(dataframe,
                         llm_api_key,
+                        # LLM parameters
                         llm_provider="openai",
                         llm_model="gpt-3.5-turbo",
                         visualization_library="seaborn",
                         goal_temperature=0.50,
                         code_generation_temperature=0.05,
+                        # Data summarization parameters
+                        data_summary_method="llm",
+                        number_of_samples_to_show_in_summary=5,
+                        return_data_fields_summary=True,
+                        # EDA parameters
                         number_of_goals_to_generate=5,
                         plot_recommended_visualization=False,
                         show_code_for_recommended_visualization=False):
@@ -44,18 +50,42 @@ def GenerateEDAWithLIDA(dataframe,
     # Create summary and goals
     summary = lida.summarize(
         data=dataframe, 
-        summary_method="default", 
-        textgen_config=goal_textgen_config
+        summary_method=data_summary_method,
+        textgen_config=goal_textgen_config,
+        n_samples=number_of_samples_to_show_in_summary
     )
     goals = lida.goals(
         summary, 
         n=number_of_goals_to_generate,
         textgen_config=goal_textgen_config
     )
+    
+    # Print summary of the data
+    if data_summary_method == "llm":
+        print("-----  DATA SUMMARY  -----")
+        # Get name of dataset from summary JSON
+        print("Name:", summary['name'])
+        print("Description:", summary['dataset_description'])
+        
+        # Convert fields to dataframe
+        fields = pd.DataFrame(summary['fields'])
+        
+        # The properties column is a dictionary, so convert it to columns
+        properties = pd.DataFrame(fields['properties'].tolist())
+        fields = pd.concat([fields, properties], axis=1)
+        fields = fields.drop(columns=['properties'])
+        
+        # Set column as index
+        fields = fields.set_index('column')
+        
+        # Move description column to the front
+        fields = fields[['description'] + [col for col in fields.columns if col != 'description']]
+            
 
     # Create charts for goals
+    print("-----  EXPLORATORY DATA ANALYSIS (EDA) GOALS  -----")
     for i in range(number_of_goals_to_generate):
-        print("Exploratory Data Analysis Goal/Question #" + str(i+1) + ":", goals[i].question)
+        print("Question #" + str(i+1) + ":", goals[i].question)
         print("Why:", goals[i].rationale)
         if plot_recommended_visualization or show_code_for_recommended_visualization:
             # Generate charts
@@ -77,6 +107,10 @@ def GenerateEDAWithLIDA(dataframe,
             # Describe the recommended visualization
             print("Recommended visulization:", goals[i].visualization)
         print("\n")
+    
+    # Return data summary dataframe if requested
+    if return_data_fields_summary:
+        return fields
 
 
 # # Test function
@@ -87,5 +121,5 @@ def GenerateEDAWithLIDA(dataframe,
 # GenerateEDAWithLIDA(
 #     dataframe=iris,
 #     llm_api_key = open("C:/Users/oneno/OneDrive/Desktop/OpenAI key.txt", "r").read(),
-#     number_of_goals_to_generate=6
+#     number_of_goals_to_generate=1
 # )
