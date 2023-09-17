8dba1f4fc20ae871d13fa58b0397e3035dbce492
Kyle Protho <onenonlykpro@gmail.com>
2023-09-17 14:50:54 -0400

    Update ConvertOddsToProbability.py

--- Python/Data Processing/ConvertOddsToProbability.py
+++ Python/Data Processing/ConvertOddsToProbability.py
@@ -4,18 +4,18 @@ import pandas as pd
 
 # Declare function
 def ConvertOddsToProbability(dataframe,
-                             odds_columns,
+                             odds_column,
                              probability_column_name=None):
     
     # If probability column name is not specified, set it to odds column name + "- as probability"
     if probability_column_name is None:
-        probability_column_name = odds_columns + " - as probability"
+        probability_column_name = odds_column + " - as probability"
     
     # Convert odds to probability
     dataframe[probability_column_name] = np.where(
-        (dataframe[odds_columns].isnull()) | (dataframe[odds_columns]+1 == 0),
+        (dataframe[odds_column].isnull()) | (dataframe[odds_column]+1 == 0),
         np.nan,
-        dataframe[odds_columns] / (1 + dataframe[odds_columns])
+        dataframe[odds_column] / (1 + dataframe[odds_column])
     )
     
     # Return updated dataframe
