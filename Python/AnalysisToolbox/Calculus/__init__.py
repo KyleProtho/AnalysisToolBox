f95e01572028f47249315ca9a3eb9ea3e8546522
Kyle Protho <Onenonlykpro@gmail.com>
2023-11-06 07:53:30 -0500

    Reorg

--- Python/AnalysisToolbox/Calculus/__init__.py
+++
@@ -1,1 +0,0 @@
-from Calculus import *
--- Python/AnalysisToolbox/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-PACKAGE_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-
-# Import all of the modules in the repository into a single namespace
-from analysistoolbox import *
-
