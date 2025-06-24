439a12b8a53ff73beaf24368dcc8362646f586f6
Kyle Protho <Onenonlykpro@gmail.com>
2025-06-24 12:58:37 -0400

    pymetalog fix attempt

--- analysistoolbox/simulations/pymetalog/__init__.py
+++ analysistoolbox/simulations/pymetalog/__init__.py
@@ -4,12 +4,12 @@ from .class_method import rmetalog, plot, qmetalog, pmetalog, dmetalog, summary,
 # Create a pymetalog module that can be imported
 class pymetalog:
     def __init__(self):
-        self.metalog = metalog
-        self.rmetalog = rmetalog
-        self.plot = plot
-        self.qmetalog = qmetalog
-        self.pmetalog = pmetalog
-        self.dmetalog = dmetalog
-        self.summary = summary
-        self.update = update
+        self.metalog = metalog()
+        self.rmetalog = rmetalog()
+        self.plot = plot()
+        self.qmetalog = qmetalog()
+        self.pmetalog = pmetalog()
+        self.dmetalog = dmetalog()
+        self.summary = summary()
+        self.update = update()
 
